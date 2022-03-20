a07ccf359528e757366362f14c305ffa76dab946
Dwi Syiamnuari <33597747+arikjunior@users.noreply.github.com>
2022-03-21 01:27:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,36 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
   });
 
   // toggle menu navbar script
   $(".menu-button").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-button i").toggleClass("active");
+  });
+
+  // typing animation
+  var typed = new Typed(".typing", {
+    strings: ["Junior Web Developer", "Junior Frontend", "Junior Backend", "Designer", "IT Support"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Junior Web Developer", "Junior Frontend", "Junior Backend", "Designer", "IT Support"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
   });
 
   // owl carousel script
