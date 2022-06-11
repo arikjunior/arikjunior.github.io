0b7d4229487e731a78d98fa90ec904c618b214a8
Dwi Syiamnuari <33597747+arikjunior@users.noreply.github.com>
2022-06-12 00:32:54 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+const calculatorScreen = document.querySelector(".calculator-screen")
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+
+
+// numbers.forEach((number) => {
+//     number.addEventListener("click", (event) => {
+//         inputNumber(event.target.value)
+//         updateScreen(currentNumber)
+//     })
+// })
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    prevNumber = currentNumber
+    calculationOperator = operator
+    currentNumber = ''
+}
